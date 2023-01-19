742bde928aaf7174dd64cb553faee86fcfb97715
pasinduanuradhaperera <102132643+pasinduanuradhaperera@users.noreply.github.com>
2023-01-20 01:16:11 +0530

    some changes

---
+++ script.js
@@ -0,0 +1,40 @@
+function formValidate(){
+  console.log("dsfdfdsf");
+ alert("pasindu");
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var email = document.getElementById("email").value;
+    var DoB = document.getElementById("DoB").value;
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+
+    
+    if(fname == ""){
+        alert("First Name should fill");
+        return false;
+
+    }
+    console.log("dsfdfdsf");
+    if(lname == ""){
+        alert("Last Name should fill");
+        return false;
+        
+    }
+    if(email == ""){
+        alert("Email should fill");
+        return false;
+        
+    }
+    console.log("dsfdfdsf");
+    if(password == ""){
+        alert("Password should fill");
+        return false;
+        if(confirmPassword != password){
+            alert("Password not match");
+            return false;
+            
+        }
+    }
+    return true;
+    
+}
